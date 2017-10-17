a37571056c75b84c05fec618b81a81e4f9e6d489
João Paulo <joao_paulosg@outlook.com>
2017-10-17 19:21:50 -0200

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private float moveSpeedStore;
+	public float speedMultiplier;
+	public float speedIncreaseMilestone;
+	private float speedIncreaseMilestoneStore;
+	private float speedMilestoneCount;
+	private float speedMilestoneCountStore;
+
+	public float jumpForce;
+	public float jumpTime;
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+
+	private Rigidbody2D myRigidbody;
+	//private Collider2D myCollider;
+
+	private Animator myAnimator;
+	
+	private float jumpTimeCounter;
+
+	public GameManager theGameManager;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+		//myCollider = GetComponent<BoxCollider2D> ();
+		myAnimator = GetComponent<Animator> ();
+		jumpTimeCounter = jumpTime;
+		speedMilestoneCount = speedIncreaseMilestone;
+
+		moveSpeedStore = moveSpeed;
+		speedMilestoneCountStore = speedMilestoneCount;
+		speedIncreaseMilestoneStore = speedIncreaseMilestone;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+		if (transform.position.x > speedMilestoneCount) {
+			speedIncreaseMilestone += speedIncreaseMilestone * speedMultiplier;
+			speedMilestoneCount += speedIncreaseMilestone;
+			moveSpeed = moveSpeed * speedMultiplier;
+			 
+		}
+
+		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
+
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			if (grounded) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+			}
+		}
+
+		if (Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) {
+			if (jumpTimeCounter > 0) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+				jumpTimeCounter -= Time.deltaTime;
+			}
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space) || Input.GetMouseButtonUp (0)) {
+			jumpTimeCounter = 0;
+		}
+
+		if (grounded) {
+			jumpTimeCounter = jumpTime;
+		}
+
+		myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
+		myAnimator.SetBool ("Grounded", grounded);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "KillBox") {
+			theGameManager.RestartGame ();
+			moveSpeed = moveSpeedStore;
+			speedMilestoneCount = speedMilestoneCountStore;
+			speedIncreaseMilestone = speedIncreaseMilestoneStore;
+		}
+	}
+
+}
