125f8d3469cfb6630b58de5d959c450490aecf4f
João Paulo Serodio <joao_paulosg@icloud.com>
2017-11-05 15:22:21 -0200

    Adicionado matéria escura

---
+++ Assets/Scripts/ObjectRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour {
+
+	Vector3 rotationEuler;
+
+	void Update(){
+		rotationEuler -= Vector3.forward*360*Time.deltaTime; //increment 360 degrees every second
+		transform.rotation = Quaternion.Euler(rotationEuler);
+		//To convert Quaternion -> Euler, use eulerAngles
+		//print(transform.rotation.eulerAngles);
+	}
+}
